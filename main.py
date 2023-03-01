00c5e44050aed293f6c64f57eaecdf8472299f5c
George Goodey <george.goodey@student.manchester.ac.uk>
2023-03-01 15:57:59 +0000

    Faster files

--- main.py
+++ main.py
@@ -1,40 +1,73 @@
-import csv 
-import pymongo
-import datetime
+import time
+import csv
+from pymongo import MongoClient
+from multiprocessing import Pool
 
-mongoClient = pymongo.MongoClient("mongodb://192.168.56.1:27017/")
-print(mongoClient.list_database_names())
-header = ['name', 'item', 'date']
-db = mongoClient['bet365']
-collection = db['customers']
+def cmt():
+    return round(time.time() * 1000)
 
-csvFile = open('customer_orders.csv', 'r')
+def getData():
+    dataStart = cmt()
+    header = ['name', 'item', 'date']
+    csvFile = open('customer_orders.csv', 'r')
 
-startTime = datetime.datetime.now()
+    dbData = []
 
-with open('customer_orders.csv','r') as csvfile:
-    data = csv.reader(csvfile, delimiter = ',')
-    *_, last = data
+    with open('customer_orders.csv','r') as csvfile:
+        data = csv.reader(csvfile, delimiter = ',')
+        *_, last = data
 
-with open('customer_orders.csv','r') as csvfile:
-    # print(last)
-    data = csv.reader(csvfile, delimiter = ',')
-    csvRow = next(data)
-    for n in range(10):
-    # while(csvRow != last):
+    with open('customer_orders.csv','r') as csvfile:
+        data = csv.reader(csvfile, delimiter = ',')
+        csvRow = next(data)
+        
+        while(csvRow != last):
+            row = {}
+            for i in header:
+                row[i] = csvRow[header.index(i)]
+            dbData.append(row)
+            csvRow = next(data)
         row = {}
         for i in header:
             row[i] = csvRow[header.index(i)]
-        collection.insert_one(row)
-        csvRow = next(data)
-    row = {}
-    for i in header:
-        row[i] = csvRow[header.index(i)]
-    # collection.insert_one(row)
+        dbData.append(row)
+        dataEnd = cmt()
+        dataSortTime = dataEnd - dataStart
+        print(f"Sorted database In "+str(dataSortTime)+" Millis")
+        return dbData
 
-# collection.findOne()
+def startDatabase():
+    mongoClient = MongoClient("mongodb://localhost:27017")
+    db = mongoClient.bet365
+    if 'customers' in db.list_collection_names():
+        collection = db['customers']
+        collection.drop()
+    collection = db.customers
 
-endTime = datetime.datetime.now()
-timePassed = endTime - startTime
-print(f"Time taken: "+str(timePassed.microseconds))
+def insertData(dbData):
+    mongoClient = MongoClient("mongodb://localhost:27017")
+    db = mongoClient.bet365
+    collection = db.customers
+    collection.insert_many(dbData)
+
+if __name__ == '__main__':
+    sTime = cmt()
+
+    print("Starting Program")
+    dTime = 0
+    data = getData()
+    pool = Pool(processes=8)
+    chunkSize = len(data)//8
+    chunks = [data[i:i+chunkSize] for i in range(0, len(data), chunkSize)]
+
+    startDatabase()
+    pool.map(insertData, chunks)
+
+    pool.close()
+    pool.join()
+    eTime = cmt()
+    timePassed = eTime - sTime
+    print(f"Total Time Taken: "+str(timePassed)+" Millis")
+
+
     #mongodb+srv://Admin:guHURrbGhWRsfERb@cluster0.gs3a8t9.mongodb.net/test
